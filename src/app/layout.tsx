2484e219f9d87c5d89c343ac8001fe32b103b65f
Roman Mik <it.mikroman@gmail.com>
2026-08-14 23:38:34 +0200

    chore: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Caprasimo, Figtree } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css';
 
 const figtree = Figtree({
@@ -29,6 +30,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
       <body className="min-h-full flex flex-col font-body">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
